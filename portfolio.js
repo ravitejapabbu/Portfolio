9cfd1ecbd71e9122728453b9752afbf62cc0a54a
ravitejapabbu <164178902+ravitejapabbu@users.noreply.github.com>
2025-05-22 12:35:51 +0530

    Changing Roles

--- portfolio.js
+++ portfolio.js
@@ -8,3 +8,27 @@ document.querySelector("#menu-close").onclick = () =>{
 window.onscroll = () =>{
     menu.classList.remove("active");
 }
+
+
+
+
+const roles = ["Software Engineer", "Full Stack Developer", "Data Scientist", "ML&AI  Expert", "Cloud Engineer"];
+  let roleIndex = 0;
+  const typingSpeed = 110;   // ms per character
+  const delayBetweenRoles = 2000; // ms wait after typing a role
+const span = document.getElementById("role");
+
+  function typeText(text, index = 0) {
+    if (index < text.length) {
+      span.textContent += text.charAt(index);
+      setTimeout(() => typeText(text, index + 1), typingSpeed);
+    } else {
+      setTimeout(() => {
+        span.textContent = "";  // Clear for next word
+        roleIndex = (roleIndex + 1) % roles.length;
+        typeText(roles[roleIndex]);
+      }, delayBetweenRoles);
+    }
+  }
+
+  typeText(roles[roleIndex]);
